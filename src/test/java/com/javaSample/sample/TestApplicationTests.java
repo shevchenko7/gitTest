3727871dd5e2486ba4e4a2e1ecf1d9bd724e20eb
황형훈 <messi09@wemakeprice.com>
2019-12-13 15:24:04 +0900

    java sample upload

---
+++ src/test/java/com/javaSample/sample/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaSample.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
